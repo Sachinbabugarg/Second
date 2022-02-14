1e3e37d1d84e340cf9c32a830594f5f0137ccc66
Sachinbabugarg <sachinbabugarg2006@gmail.com>
2022-02-14 11:21:13 +0530

    vextor

---
+++ VecTor.java
@@ -0,0 +1,19 @@
+package Vec;
+import java.util.*;
+public class VecTor {
+public static void main(String[]args)
+{
+	Vector v=new Vector();
+	System.out.println(v.capacity());
+	for(int i=1;i<=10; i++)
+	{
+		v.addElement(i);
+	}
+	System.out.println(v);
+	System.out.println(v.capacity());
+	v.addElement("11");
+	System.out.println(v);
+	System.out.println(v.capacity());
+	
+}
+}
